f02d2e48040a726c0d8f89ab864edb41b687a5a0
jrisk <joeyrsk@gmail.com>
2015-04-13 16:47:49 -0400

    comments and tracking

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page.
+// GET home page.
 router.get('/', function(req, res) {
 res.render('index', {title: "Express"});
-});
-*/
-
-router.get('/', function(req, res) {
-res.send('ok')
 });
 
 module.exports = router;
