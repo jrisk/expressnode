93d457730868f25de4f7096682e05ed568ca2b03
jrisk <joeyrsk@gmail.com>
2015-04-14 17:59:09 -0400

    routing with user gets

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-// GET home page.
+/* GET home page.
 router.get('/', function(req, res) {
 res.render('index', {title: "Express"});
+}); 
+
+router.get('/users/:id', function(req, res) {
+	console.log(req.params);
+	res.send(req.params.id, 200);
 });
+
+*/
+router.get('/', function(req, res) {
+	res.render('index', {
+		title: 'My App!',
+		age: 25,
+		skill: 'node.js'
+	});
+});
+
+
+
 
 module.exports = router;
